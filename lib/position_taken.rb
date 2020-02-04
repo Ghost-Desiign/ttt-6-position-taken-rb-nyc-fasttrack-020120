5bfd9ce12431d546968d898ff9c8588e0d33e8aa
Brandon Lee <eternalartistxii@yahoo.com>
2020-02-04 06:24:09 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,10 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  #false???
   if (board[index] == "X" || board[index] == "0")
-    
+    return true
   end
-end
+  
+  return false
+end 
+
